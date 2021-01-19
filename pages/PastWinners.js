7246de3e8e8d3f748bb7677625a8433e2f93ee26
r-jo <rdrsjck+gh@gmail.com>
2021-01-19 10:21:06 -0800

    Smoothened shadows and skewed cards

--- pages/PastWinners.js
+++ pages/PastWinners.js
@@ -9,37 +9,59 @@ export default function PastWinners() {
     <main className={Winnerstyles.main}>
       <section>
         <h1 className={styles.title}>Past Winners</h1>
-        <div className={Introstyles.cardContainer}>
-          <Card
-            picture="/placeholder.jpg"
-            title="The Mandolorian"
-            caption="First place winner"
-            description="After the stories of Jango and Boba Fett, another warrior emerges in the Star Wars universe. “The Mandalorian” is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy, far from the authority of the New Republic."
-          />
-          <Card
-            picture="/placeholder.jpg"
-            title="The Mandolorian"
-            caption="First place winner"
-            description="After the stories of Jango and Boba Fett, another warrior emerges in the Star Wars universe. “The Mandalorian” is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy, far from the authority of the New Republic."
-          />
-          <Card
-            picture="/placeholder.jpg"
-            title="The Mandolorian"
-            caption="First place winner"
-            description="After the stories of Jango and Boba Fett, another warrior emerges in the Star Wars universe. “The Mandalorian” is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy, far from the authority of the New Republic."
-          />
-          <Card
-            picture="/placeholder.jpg"
-            title="The Mandolorian"
-            caption="First place winner"
-            description="After the stories of Jango and Boba Fett, another warrior emerges in the Star Wars universe. “The Mandalorian” is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy, far from the authority of the New Republic."
-          />
-          <Card
-            picture="/placeholder.jpg"
-            title="The Mandolorian"
-            caption="First place winner"
-            description="After the stories of Jango and Boba Fett, another warrior emerges in the Star Wars universe. “The Mandalorian” is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy, far from the authority of the New Republic."
-          />
+        <div className={Winnerstyles.cardContainer}>
+          <div className={Winnerstyles.section}>
+            <div className={Winnerstyles.first}>
+              <Card
+                picture="/placeholder.jpg"
+                title="The Mandolorian"
+                caption="First place winner"
+                description="After the stories of Jango and Boba Fett, another warrior emerges in the Star Wars universe. “The Mandalorian” is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy, far from the authority of the New Republic."
+              />
+            </div>
+            <div className={Winnerstyles.second}>
+              <Card
+                picture="/placeholder.jpg"
+                title="The Mandolorian"
+                caption="First place winner"
+                description="After the stories of Jango and Boba Fett, another warrior emerges in the Star Wars universe. “The Mandalorian” is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy, far from the authority of the New Republic."
+              />
+            </div>
+            <div className={Winnerstyles.third}>
+              <Card
+                picture="/placeholder.jpg"
+                title="The Mandolorian"
+                caption="First place winner"
+                description="After the stories of Jango and Boba Fett, another warrior emerges in the Star Wars universe. “The Mandalorian” is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy, far from the authority of the New Republic."
+              />
+            </div>
+          </div>
+          <div className={Winnerstyles.section}>
+            <div className={Winnerstyles.fourth}>
+              <Card
+                picture="/placeholder.jpg"
+                title="The Mandolorian"
+                caption="First place winner"
+                description="After the stories of Jango and Boba Fett, another warrior emerges in the Star Wars universe. “The Mandalorian” is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy, far from the authority of the New Republic."
+              />
+            </div>
+            <div className={Winnerstyles.fifth}>
+              <Card
+                picture="/placeholder.jpg"
+                title="The Mandolorian"
+                caption="First place winner"
+                description="After the stories of Jango and Boba Fett, another warrior emerges in the Star Wars universe. “The Mandalorian” is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy, far from the authority of the New Republic."
+              />
+            </div>
+            <div className={Winnerstyles.sixth}>
+              <Card
+                picture="/placeholder.jpg"
+                title="The Mandolorian"
+                caption="First place winner"
+                description="After the stories of Jango and Boba Fett, another warrior emerges in the Star Wars universe. “The Mandalorian” is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy, far from the authority of the New Republic."
+              />
+            </div>
+          </div>
         </div>
       </section>
     </main>
