f748233966a2e80f3003b01d07fb1c67081f11cf
claynaut <jpesc002@ucr.edu>
2021-01-20 22:40:41 -0800

    Added color variables

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 // Removes default focus border because its ugly
 import 'what-input';
+import '../styles/themes.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
