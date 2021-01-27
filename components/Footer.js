c2e7c006392bd50d4d442539a2abd6a149c63cab
claynaut <jpesc002@ucr.edu>
2021-01-27 09:58:20 -0800

    Added team apge component, tweaked styling

--- components/Footer.js
+++ components/Footer.js
@@ -1,22 +1,9 @@
 import styles from "../styles/Common.module.css";
-import {FaFacebookSquare, FaInstagramSquare, FaLinkedin} from 'react-icons/fa';
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <div className ={styles.contact}>
-        <a href="https://www.facebook.com/UCRBioHack/"><FaFacebookSquare/></a>
-        <a href="https://www.instagram.com/biohack_ucr/?hl=en"><FaInstagramSquare/></a>
-        <a href="https://www.linkedin.com/company/biohackucr/"><FaLinkedin/></a>
-      </div>
-      <div className={styles.contactlink}>
-        <a href="mailto:biohack.ucr@gmail.com">
-          biohack.ucr@gmail.com
-        </a>
-      </div>
-      <div className={styles.teamlink}>
-        Powered by love and coffee from the Biohack team.
-      </div>
+      Powered by love and coffee from the Biohack team.
     </div>
   );
 }
