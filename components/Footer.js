3256abecc8c7fa6d1379fe755c0bdc2c143e3093
claynaut <jpesc002@ucr.edu>
2021-01-26 22:05:10 -0800

    Installed react icons, added contact/social media links to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,22 @@
 import styles from "../styles/Common.module.css";
+import {FaFacebookSquare, FaInstagramSquare, FaLinkedin} from 'react-icons/fa';
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+    <div className={styles.footer}>
+      <div className ={styles.contact}>
+        <a href="https://www.facebook.com/CitrusHack/"><FaFacebookSquare/></a>
+        <a href="https://twitter.com/citrushack?lang=en"><FaInstagramSquare/></a>
+        <a href="https://www.instagram.com/citrushack_ucr/?hl=en"><FaLinkedin/></a>
+      </div>
+      <div className={styles.contactlink}>
+        <a href="mailto:biohack.ucr@gmail.com">
+          biohack.ucr@gmail.com
+        </a>
+      </div>
+      <div className={styles.teamlink}>
         Powered by love and coffee from the Biohack team.
-      </a>
-    </footer>
+      </div>
+    </div>
   );
 }
