a7bd4749a7037a471a4ce5113fd312441a27710a
r-jo <rdrsjck+gh@gmail.com>
2021-02-04 09:00:18 -0800

    Added phage

--- pages/PastWinners.js
+++ pages/PastWinners.js
@@ -13,7 +13,7 @@ export default function PastWinners() {
           <div className={Winnerstyles.cell}>
             <p className={Winnerstyles.text}>
               <b>Here are the 2019 BioHack Winners!</b> Hover over the polaroids
-              to learn more about their project!
+              to learn more about each project!
             </p>
           </div>
           <div className={Winnerstyles.cell}>
@@ -71,12 +71,7 @@ export default function PastWinners() {
             />
           </div>
           <div className={Winnerstyles.cell}>
-            <Image
-              src="/placeholder.jpg"
-              alt="Filler"
-              width={200}
-              height={200}
-            />
+            <div className={Winnerstyles.phage}></div>
           </div>
         </div>
       </section>
